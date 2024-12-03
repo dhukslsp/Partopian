89571b9f4402475435ec0306357bb0d2c35a2ce6
Dhruv Singh (dhukslsp) <dhruv1647.singh@gmail.com>
2024-12-04 00:18:46 +0530

    This part is Commpted12

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -3,16 +3,16 @@ import './Navbar.css'
 const Navbar = () => {
     return (
         <nav className="navbar">
-            <div className="logo">MyWebsite</div>
+            <div className="logo"><img src="he-removebg-preview.png" alt="" srcset=""  style = {{height:"5pc",marginTop:"2pc"}}/></div>
             <ul className="nav-links">
                 <li><a href="#home">Home</a></li>
                 <li><a href="#about">Product</a></li>
                 <li><a href="#services">Testimonials</a></li>
                 <li><a href="#contact">Contact</a></li>
             </ul>
-            <ul className="nav-links">
+            <ul className="nav-links" >
                 <li><a href="#home">Login</a></li>
-                <li><a href="#about">Signup</a></li>
+                <li><a href="#about" style={{background:"black",color:"white",borderRadius:"20px"}}>Getting Started</a></li>
             </ul>
         </nav>
     );
