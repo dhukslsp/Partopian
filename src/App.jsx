f168990e0af905dca544ee891be7225ccbb744d7
Dhruv Singh (dhukslsp) <dhruv1647.singh@gmail.com>
2024-12-03 19:30:19 +0530

    New work

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <div class="line horizontal-line"></div>
+        <div class="line horizontal-line2"></div>
+        <div class="line horizontal-line23"></div>
+        <div class = "personContainer">
+          <img src="./person1.webp" alt="" srcset="" />
+        </div>
+        <div class = "personContainer1">
+          <img src="./person1.webp" alt="" srcset="" />
+        </div>
+        <div class = "personContainer2">
+          <img src="./person1.webp" alt="" srcset="" />
+        </div>
+        <div class = "personContainer3">
+          <img src="./person1.webp" alt="" srcset="" />
+        </div>
+        <div class = "maincontent">
+          <h1 class = "title1">A World built on <br/> RESARCH <br/> not perfection</h1>
+          <img src="./continents.jpg" alt="" srcset="" style = {{width:"100%",height:"80vh"}}/>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
