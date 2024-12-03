cb3ad51431de90d086e79bdb7229a8b91edf5341
Dhruv Singh (dhukslsp) <dhruv1647.singh@gmail.com>
2024-12-04 00:24:28 +0530

    This part is Commpted12

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -2,19 +2,22 @@ import React from 'react'
 import './Navbar.css'
 const Navbar = () => {
     return (
-        <nav className="navbar">
-            <div className="logo"><img src="he-removebg-preview.png" alt="" srcset=""  style = {{height:"5pc",marginTop:"2pc"}}/></div>
-            <ul className="nav-links">
-                <li><a href="#home">Home</a></li>
-                <li><a href="#about">Product</a></li>
-                <li><a href="#services">Testimonials</a></li>
-                <li><a href="#contact">Contact</a></li>
-            </ul>
-            <ul className="nav-links" >
+        <div id = "space">
+            <nav className="navbar">
+                <div className="logo"><img src="he-removebg-preview.png" alt="" srcset="" style={{ height: "5pc", marginTop: "2pc" }} /></div>
+                <ul className="nav-links">
+                    <li><a href="#home">Home</a></li>
+                    <li><a href="#about">Product</a></li>
+                    <li><a href="#services">Testimonials</a></li>
+                    <li><a href="#contact">Contact</a></li>
+                </ul>
+
+            </nav>
+            <ul className="nav-links" style = {{marginTop:"21px"}}>
                 <li><a href="#home">Login</a></li>
-                <li><a href="#about" style={{background:"black",color:"white",borderRadius:"20px"}}>Getting Started</a></li>
+                <li><a href="#about" style={{ background: "black", color: "white", borderRadius: "20px" }}>Getting Started</a></li>
             </ul>
-        </nav>
+        </div>
     );
 };
 
